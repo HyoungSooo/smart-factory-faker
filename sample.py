e229e3da28d54b653be60bb201d371c48c2d7add
Hyoungsooo <aaa57403@gmail.com>
2023-07-27 15:01:41 +0900

    V 0.4.1 update

--- sample.py
+++ sample.py
@@ -4,6 +4,7 @@ from factory.sensor import Sensor, BoolSensor
 from process.gate import *
 from process.processor import Processor
 from process.processor_stack import CallStackProcessor
+from miner.visualize import ProcessVisualize
 
 fa1 = Facility('test1', Sensor('test_sensor1', 1, 0),
                BoolSensor('fa1 test_sessor', 0.2))
@@ -15,31 +16,20 @@ fa6 = Facility('test6', Sensor('test_sensor6', 1, 0))
 fa7 = Facility('test7', Sensor('test_sensor7', 1, 0))
 fa8 = Facility('test8', Sensor('test_sensor8', 1, 0))
 
-k = Processor(start_node=fa1,
-              route={
-                  fa1.name: Or([0, 1], [fa2, fa3]),
-                  fa2.name: SeqLoop(fa4),
-                  fa3.name: SeqLoop(fa4),
-                  fa4.name: SeqLoop(fa5),
-                  fa5.name: Or([0, 1], node=[fa6, fa7]),
-                  fa6.name: SeqLoop(fa5),
-                  fa7.name: None
-              })
+route = {
+    fa1.name: Or([0, 1], [fa2, fa3]),
+    fa2.name: SeqLoop(fa4),
+    fa3.name: SeqLoop(fa4),
+    fa4.name: SeqLoop(fa5),
+    fa5.name: Or([0, 1], node=[fa6, fa7]),
+    fa6.name: SeqLoop(fa5),
+    fa7.name: None
+}
 
 j = CallStackProcessor(start_node=fa1,
-                       route={
-                           fa1.name: Or([0, 1], [fa2, fa3]),
-                           fa2.name: SeqLoop(fa4),
-                           fa3.name: SeqLoop(fa4),
-                           fa4.name: SeqLoop(fa5),
-                           fa5.name: Or([0, 1], node=[fa6, fa7]),
-                           fa6.name: SeqLoop(fa5),
-                           fa7.name: None
-                       })
+                       route=route)
 
 print(j.logs)
 print(j.to_csv(100, './test.csv'))
-# df = k.to_dataframe(10)
-# df_to_csv = k.to_csv(10, path='./res.csv')
-# df_by_fa = k.by_facility(100)
-# print(df_by_fa)
+ProcessVisualize('./', 'test', 'text graph',
+                 'test').visuallizer(fa1, route=route)
