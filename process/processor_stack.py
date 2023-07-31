399867c22d4ab269c213e9b8cb3aa2a955c19155
Hyoungsooo <aaa57403@gmail.com>
2023-07-31 15:48:16 +0900

    update tutorial

--- process/processor_stack.py
+++ process/processor_stack.py
@@ -38,7 +38,6 @@ class CallStackProcessor(BaseProcessor):
             else:
                 heapq.heappush(
                     token_stack, [time + 1, token_id, gate])
-        print(node.sensor[0]._get_data_generator())
 
     def _set_start_token(self, iter):
         stack = []
